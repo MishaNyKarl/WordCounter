8e3c2b87b226c6976b46ac2ba589fa703fa30f8d
Michael Karlyavin <karlyavinmisha@gmail.com>
2024-01-25 17:35:52 +0300

    Добавьте файлы проекта.

---
+++ test_1/test_1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <tuple>
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	
+	std::unordered_map<std::string, int> freqs;
+	std::string word;
+	while (std::cin >> word) {
+		if (word == "stop") break;
+		++freqs[word];
+	}
+
+	std::vector<std::pair<std::string, int>> sortedByFreq(
+		freqs.begin(),
+		freqs.end()
+	);
+
+	std::sort(
+		sortedByFreq.begin(),
+		sortedByFreq.end(),
+		[](const auto& p1, const auto& p2) {
+			return std::tie(p2.second, p1.first) < std::tie(p1.second, p1.first);
+		}
+	);
+
+	for (const std::pair<const std::string, int>& freq : sortedByFreq) {
+		std::cout << freq.first << "\t" << freq.second << "\n";
+	}
+}
